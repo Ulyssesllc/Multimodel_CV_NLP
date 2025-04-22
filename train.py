be2f25a7c04d3aa1de087764900f636e3873a524
DucCuong12 <duccuongtoan@gmail.com>
2025-04-22 23:52:32 +0700

    args

--- train.py
+++ train.py
@@ -4,25 +4,17 @@ import torch.optim as optim
 import torch.nn as nn
 from process_data import MyData 
 from torch.utils.data import random_split
-
-from sklearn.model_selection import train_test_split
 from torch.utils.data import DataLoader
-# def __init__(self, csv_file, img_file):
-csv_file = "training_data.csv"
-img_dir = "amazon_dataset"
-dataset = MyData(csv_file, img_dir)
 
 
-train_size = int(0.8 * len(dataset))
-test_size = len(dataset) - train_size
-train_dataset, test_dataset = random_split(dataset, [train_size, test_size])
+def train(model, csv_file, img_dir, epochs):
+    dataset = MyData(csv_file, img_dir) 
+    train_size = int(0.8 * len(dataset))
+    test_size = len(dataset) - train_size
+    train_dataset, test_dataset = random_split(dataset, [train_size, test_size])
 
-train_data = DataLoader(train_dataset, batch_size= 8, shuffle= True)
-test_data = DataLoader(test_dataset, batch_size = 8, shuffle= False )
-
-model = FusionModule()
-
-def train(model, dataloader, epochs):
+    train_data = DataLoader(train_dataset, batch_size= 8, shuffle= True)
+    test_data = DataLoader(test_dataset, batch_size = 8, shuffle= False )
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     model.to(device)
     criterion = nn.CrossEntropyLoss()
@@ -53,7 +45,7 @@ def train(model, dataloader, epochs):
         model.train()
         total_loss = 0 
         correct = 0
-        for batch in dataloader:
+        for batch in train_data:
             img = batch['image'].to(device)
             input_ids = batch['input_ids'].to(device)
             attention_mask = batch['attention_mask'].to(device)
